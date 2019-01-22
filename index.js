1669a769909201c5d5893851dc5f0e8550d00327
hivaids2512 <tranquy2512@gmail.com>
2019-01-22 20:42:00 +0700

    Added merge sort and heap sort

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const SelectionSort = require('./sortable/selection');
 const InsertionSort = require('./sortable/insertion');
+const MergeSort = require('./sortable/merge');
+const HeapSort = require('./sortable/heap');
 
 module.exports = {
   SelectionSort,
   InsertionSort,
+  MergeSort,
+  HeapSort,
 };
